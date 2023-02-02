00c22048ac62f3c15ab5f18837ccd76cc2fa8b99
littvs <90873121+littvs@users.noreply.github.com>
2023-02-02 10:55:44 -0500

    Create Matlab.m

---
+++ Matlab.m
@@ -0,0 +1,31 @@
+---
+layout: default
+title: Data Visualization with Matlab
+nav_order: 11
+---
+
+## Data Visualization with Matlab
+
+<img src="assets/img/Matlab-Final.png" alt="Workshop Title Slide" width="720">
+
+This asynchronous module discusses how you can create publication-quality figures using MATLAB or its free alternative, Octave. The module covers plotting options for one- and two-dimensional continuous and categorical data and explore options for customizing your figure's appearance. 
+
+Presentation by Subhanya Sivajothy (Data Analysis and Visualization Librarian).
+
+[Book an appointment with Isaac or another member of the DASH Team.](https://library.mcmaster.ca/services/dash)
+
+# Workshop preparation 
+
+Preparation for this tutorial consists of two steps: [Getting the data](#get-the-data) and [Getting the software](#get-the-software). Follow the steps below. 
+  
+## Get the data
+Access the data file [in .xlsx extension](https://mcmasteru365-my.sharepoint.com/:f:/g/personal/littvs_mcmaster_ca/EsD3ng2kvL9OqNGogNshky4BmAGqsCRixEDZpdFhheR-VA?e=lMjuNh.
+
+## Get the software
+This hands-on workshop uses [**Matlab**](https://matlab.mathworks.com/), a software application for data visualization, and [**Octave**](https://octave.org/), its free alternative. 
+
+## Asynchronous Video Module
+
+<iframe height="480" width="853" allowfullscreen frameborder=0 src="https://echo360.ca/media/577394b6-26b8-4a6a-a8b1-7ba39a358693/public"></iframe>
+
+View the original [here](https://echo360.ca/media/577394b6-26b8-4a6a-a8b1-7ba39a358693/public). 
